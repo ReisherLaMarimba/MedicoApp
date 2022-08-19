2f653fc40bd0d04711a88ef16f4151f6bb357452
ReisherLaMarimba <reisher62@gmail.com>
2022-08-19 11:28:55 -0400

    Add project files.

---
+++ MedicApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicApp.Startup))]
+namespace MedicApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
